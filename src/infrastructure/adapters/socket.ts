74cb9e7f3abb23f0e9eacb1faa9833841ca3ef2a
Autex NI <dev.msobalvarro@gmail.com>
2026-08-12 10:47:27 -0600

    middleware for socket added

--- src/infrastructure/adapters/socket.ts
+++ src/infrastructure/adapters/socket.ts
@@ -1,6 +1,8 @@
 import { Server } from 'socket.io'
 import { Server as Engine, type WebSocketData } from '@socket.io/bun-engine'
 import { Service } from 'typedi'
+import { verify } from 'hono/jwt'
+import { environments } from '@/utils/constanst'
 
 @Service()
 export class SocketAdapter {
@@ -12,12 +14,56 @@ export class SocketAdapter {
     this.engine = new Engine()
 
     this.io.bind(this.engine)
+    this.registerMiddlewares()
     this.registerEvents()
+  }
+
+  private registerMiddlewares(): void {
+    this.io.use(async (socket, next) => {
+      try {
+        const authHeader = socket.handshake.headers.authorization
+        const authParam = socket.handshake.auth?.token || socket.handshake.auth?.authorization
+
+        let token = authParam || authHeader
+
+        if (token && typeof token === 'string' && token.startsWith('Bearer ')) {
+          token = token.slice(7).trim()
+        }
+
+        if (!token || typeof token !== 'string') {
+          return next(new Error('Authentication error: Token is required'))
+        }
+
+        // TODO: Agregar mas secret key si 
+        // es necesario en un futuro
+        const secrets = [
+          environments.JWT_SECRET_USER_STUDENT,
+        ].filter(Boolean) as string[]
+
+        let payload = null
+
+        for (const secret of secrets) {
+          try {
+            payload = await verify(token, secret)
+            if (payload) break
+          } catch (_) { }
+        }
+
+        if (!payload) {
+          return next(new Error('Authentication error: Invalid or expired token'))
+        }
+
+        socket.data.user = payload
+        next()
+      } catch (error) {
+        next(new Error('Authentication error: ' + (error instanceof Error ? error.message : 'Unknown error')))
+      }
+    })
   }
 
   private registerEvents(): void {
     this.io.on('connection', (socket) => {
-      console.log(socket.id, 'connected')
+      console.log(socket.id, 'connected | User:', socket.data.user)
     })
   }
 
@@ -29,3 +75,4 @@ export class SocketAdapter {
     return this.engine.handler().websocket
   }
 }
+
